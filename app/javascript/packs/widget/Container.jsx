3a906d29f3b582728b7dd02b2e54fa868eb0cdd8
zhivou <zivou4@gmail.com>
2020-11-30 20:05:56 -0700

    Widget Template with static data

---
+++ app/javascript/packs/widget/Container.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Container extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="card bg-dark text-white">
+          <div className="card-header">
+            <h1>Herriman, UT Weather</h1>
+            <div>as of 7:40 pm MST</div>
+          </div>
+        </div>
+        <div className="card-body border">
+          <div className="text-center">
+            <span className="display-1">39°</span>
+            <span className="h4">Clear</span>
+          </div>
+        </div>
+        <div className="card-foote bg-dark">
+          <button className="btn text-white btn-block">Current Weather</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Container;
